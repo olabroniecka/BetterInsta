12d2ac7dde0eb9757d60c36b89cfdc4530185843
Krushka <Krushka@MacBook-Pro-Krushka.local>
2017-10-29 14:37:26 +0100

    wip:model comment

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    name_user = models.ForeignKey(User)
+    title = models.CharField(max_length=64)
+    comment = models.TextField()
+
